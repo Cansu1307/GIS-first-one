b67f509f5a933625785e8fab6512b39fb2b7c887
Your Name <youremail@yourdomain.com>
2024-11-04 16:07:22 +0100

    new elements

--- element.js
+++ element.js
@@ -1,2 +1,9 @@
 let element = document.getElementById('imageURL');
 element.style.color = "purple";
+element.value = "hey";
+/*let element2 = document.querySelector(".entry-footer");
+element2.innerHTML = "<p> What's up? </p>";
+
+let newelement = document.createElement("p");
+newelement.textContent = "heyhey";
+element2.append (newelement);*/
