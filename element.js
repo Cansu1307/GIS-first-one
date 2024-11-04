f920a02afe5758709230a5fa06ba63765353c41b
Your Name <youremail@yourdomain.com>
2024-11-04 17:08:45 +0100

    new elements added

--- element.js
+++ element.js
@@ -1,8 +1,16 @@
-let element = document.getElementById('imageURL');
-element.style.color = "purple";
-element.value = "hey";
+let element = document.getElementById('imageURL'); /*id wurde übernommen*/
+element.style.color = "purple"; 
+element.value = "hey"; /*hey wird eingefügt*/
+
+let element3 = document.getElementById('Bearbeiten'); /*ID Bearbeiten übernommen*/
+element3.addEventListener ('click', onclick);
+function onclick (event) {
+    element3.style.color = "pink";
+}
+
+
 /*let element2 = document.querySelector(".entry-footer");
-element2.innerHTML = "<p> What's up? </p>";
+element2.innerHTML = "<p> What's up? </p>"; /*in dem html wird das ganze Text übernommen
 
 let newelement = document.createElement("p");
 newelement.textContent = "heyhey";
